e14062ae0bd35a04421ebe0b1ecbc3fa547974fc
root <root@localhost.localdomain>
2023-11-16 18:19:00 +0200

    Fixed event handling

--- markdownapp/src/TextBody.js
+++ markdownapp/src/TextBody.js
@@ -1,19 +1,33 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import P from './PlainText';
 
-export default function TextBody({width, type, value, onChange}){
-    if(type === "input"){
-        return (
-            <textarea style={width} className="text-body" value={value} onChange={onChange}>
+export default function TextBody({ width, type, value, onChange }) {
+  const handleChange = (event) => {
+    console.log('event:', event);
+    const newValue = event.target.value;
+    console.log('newValue:', newValue);
+    onChange(newValue);
+  };
 
-            </textarea>
-        );
-    }
-    else {
-        return (
-            <div style={width} className="output-text">
-                <P value={value}/>
-            </div>
-        )
-    }
+  useEffect(() => {
+    console.log('TextBody re-rendered. Value:', value);
+  }, [value]);
+
+  if (type === 'input') {
+    return (
+      <textarea
+        style={width}
+        className="text-body"
+        value={value}
+        onChange={handleChange}
+      ></textarea>
+    );
+  } else {
+    return (
+      <div style={width} className="output-text">
+        <P value={value} />
+      </div>
+    );
+  }
 }
--- markdownapp/src/WritePad.js
+++ markdownapp/src/WritePad.js
@@ -1,16 +1,18 @@
-import TextBody from "./TextBody";
-import Header from "./HeadBanner";
 import React, { useState } from 'react';
+import Header from './HeadBanner';
+import TextBody from './TextBody';
 
 export default function Editor() {
-    const [inputValue, setInputValue] = useState('');
-    const handleInputChange = (event) => {
-        setInputValue(event.target.value);
-      };
-    return (
-      <div>
-        <Header title="Editor" width={{ width: '30vw' }} />
-        <TextBody width={{ width: '29.7vw' }} type="input" value={inputValue} onChange={handleInputChange}/>
-      </div>
-    );
-  }
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (newValue) => {
+    setInputValue(newValue);
+  };
+
+  return (
+    <div>
+      <Header title="Editor" width={{ width: '30vw' }} />
+      <TextBody width={{ width: '29.7vw' }} type="input" value={inputValue} onChange={handleInputChange}/>
+    </div>
+  );
+}
